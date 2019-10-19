ae3e939f022b5a1c155864ceae8c0740b285c4b2
Ruvie Eto <ruvie.eto@gmail.com>
2019-10-18 18:40:21 -0600

    Updates

---
+++ precache-manifest.5e48ed74b19abe5274f61daa0bc48d9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31330a483d1bfb009df3d269fcb08484",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "92505a21a5da8c1a4878",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "cd8d0851c0656b065fb9",
+    "url": "/robofriends/static/css/main.ae7f472a.chunk.css"
+  },
+  {
+    "revision": "92505a21a5da8c1a4878",
+    "url": "/robofriends/static/js/2.218cf9cb.chunk.js"
+  },
+  {
+    "revision": "cd8d0851c0656b065fb9",
+    "url": "/robofriends/static/js/main.594900a2.chunk.js"
+  },
+  {
+    "revision": "8e199c7f211d8f157d3a",
+    "url": "/robofriends/static/js/runtime~main.a9498424.js"
+  }
+]);
